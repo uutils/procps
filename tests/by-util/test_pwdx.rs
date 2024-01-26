21ce64509775f160a924c653005fb893a7ff4328
Sylvestre Ledru <sylvestre@debian.org>
2024-01-26 22:30:50 +0100

    fix the comment

--- tests/by-util/test_pwdx.rs
+++ tests/by-util/test_pwdx.rs
@@ -1,4 +1,4 @@
-// This file is part of the uutils util-linux package.
+// This file is part of the uutils procps package.
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
