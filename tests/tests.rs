d59dadc46c632ac6badcb6bc569a920e0e4a8311
Sylvestre Ledru <sylvestre@debian.org>
2024-01-26 20:20:18 +0100

    remove artifacts

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,18 +5,6 @@
 #[macro_use]
 mod common;
 
-#[cfg(feature = "lscpu")]
-#[path = "by-util/test_lscpu.rs"]
-mod test_lscpu;
-
 #[cfg(feature = "pwdx")]
 #[path = "by-util/test_pwdx.rs"]
 mod test_pwdx;
-
-#[cfg(feature = "mountpoint")]
-#[path = "by-util/test_mountpoint.rs"]
-mod test_mountpoint;
-
-#[cfg(feature = "renice")]
-#[path = "by-util/test_renice.rs"]
-mod test_renice;
