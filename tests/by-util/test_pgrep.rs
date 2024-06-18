2696fa058741ee714891068b572fa4da4e845911
Krysztal Huang <krysztal.huang@outlook.com>
2024-06-18 14:39:26 +0800

    pgrep: Add tests about `pgrep` command

--- tests/by-util/test_pgrep.rs
+++ tests/by-util/test_pgrep.rs
@@ -14,3 +14,44 @@ fn test_invalid_arg() {
 fn test_pgrep() {
     new_ucmd!().arg("--help").succeeds().code_is(0);
 }
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_oldest() {
+    new_ucmd!().arg("-o").succeeds().code_is(0);
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_newest() {
+    new_ucmd!().arg("-n").succeeds().code_is(0);
+}
+
+#[test]
+fn test_not_exist_program() {
+    new_ucmd!()
+        .arg("THIS_PROGRAM_DOES_NOT_EXIST")
+        .fails()
+        .code_is(1);
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_full() {
+    new_ucmd!().arg("sh").arg("--full").succeeds().code_is(0);
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_invalid_regex() {
+    new_ucmd!().arg("{(*").arg("--exact").fails().code_is(2);
+}
+
+#[cfg(target_os = "linux")]
+#[test]
+fn test_delimiter() {
+    let binding = new_ucmd!().arg("sh").arg("-d |").succeeds();
+    let output = binding.code_is(0).stdout_str();
+
+    assert!(output.contains('|'))
+}
