6a1f30ea52055cf74848744558a493cb3cd1e420
Krysztal Huang <krysztal.huang@outlook.com>
2024-07-09 20:41:45 +0800

    pidof: Implemented basis function

--- src/uu/pidof/src/pidof.rs
+++ src/uu/pidof/src/pidof.rs
@@ -1,4 +1,5 @@
-use clap::{arg, crate_version, Arg, ArgAction, Command};
+use clap::{crate_version, Arg, ArgAction, ArgMatches, Command};
+use uu_pgrep::process::{walk_process, ProcessInformation};
 use uucore::{error::UResult, format_usage, help_about, help_usage};
 
 const ABOUT: &str = help_about!("pidof.md");
@@ -8,7 +9,61 @@ const USAGE: &str = help_usage!("pidof.md");
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
     let matches = uu_app().try_get_matches_from(args)?;
 
-    todo!()
+    let arg_program_name = matches.get_many::<String>("program-name");
+    let arg_separator = matches.get_one::<String>("d").unwrap();
+
+    if arg_program_name.is_none() {
+        uucore::error::set_exit_code(1);
+        return Ok(());
+    };
+
+    let mut collected = collect_matched_pids(&matches);
+    collected.sort_by(|a, b| b.pid.cmp(&a.pid));
+
+    let output = collected
+        .into_iter()
+        .map(|it| it.pid.to_string())
+        .collect::<Vec<_>>()
+        .join(arg_separator);
+
+    println!("{output}");
+
+    Ok(())
+}
+
+fn collect_matched_pids(matches: &ArgMatches) -> Vec<ProcessInformation> {
+    let program_name: Vec<_> = matches
+        .get_many::<String>("program-name")
+        .unwrap()
+        .cloned()
+        .collect();
+
+    let collected = walk_process().collect::<Vec<_>>();
+    let arg_omit_pid = matches
+        .get_many::<usize>("o")
+        .unwrap_or_default()
+        .copied()
+        .collect::<Vec<_>>();
+
+    let mut processed = Vec::new();
+    for mut process in collected {
+        let contains = program_name.contains(process.status().get("Name").unwrap());
+        let should_omit = arg_omit_pid.contains(&process.pid);
+
+        if contains && !should_omit {
+            processed.push(process)
+        }
+    }
+
+    let flag_s = matches.get_flag("s");
+    if flag_s {
+        match processed.first() {
+            Some(first) => vec![first.clone()],
+            None => Vec::new(),
+        }
+    } else {
+        processed
+    }
 }
 
 pub fn uu_app() -> Command {
@@ -25,12 +80,12 @@ pub fn uu_app() -> Command {
                 .required(true)
                 .index(1),
         )
-        .arg(
-            Arg::new("c")
-                .short('c')
-                .help("Return PIDs with the same root directory")
-                .action(ArgAction::SetTrue),
-        )
+        // .arg(
+        //     Arg::new("c")
+        //         .short('c')
+        //         .help("Return PIDs with the same root directory")
+        //         .action(ArgAction::SetTrue),
+        // )
         .arg(
             Arg::new("d")
                 .short('d')
@@ -46,41 +101,35 @@ pub fn uu_app() -> Command {
                 .help("Display this help text")
                 .action(ArgAction::Help),
         )
-        .arg(
-            Arg::new("n")
-                .short('n')
-                .help("Avoid using stat system function on network shares")
-                .action(ArgAction::SetTrue),
-        )
+        // .arg(
+        //     Arg::new("n")
+        //         .short('n')
+        //         .help("Avoid using stat system function on network shares")
+        //         .action(ArgAction::SetTrue),
+        // )
         .arg(
             Arg::new("o")
                 .short('o')
                 .help("Omit results with a given PID")
-                .action(ArgAction::Set)
-                .value_name("pid"),
+                .action(ArgAction::Append)
+                .value_name("omitpid"),
         )
-        .arg(
-            Arg::new("q")
-                .short('q')
-                .help("Quiet mode. Do not display output")
-                .action(ArgAction::SetTrue),
-        )
+        // .arg(
+        //     Arg::new("q")
+        //         .short('q')
+        //         .help("Quiet mode. Do not display output")
+        //         .action(ArgAction::SetTrue),
+        // )
         .arg(
             Arg::new("s")
                 .short('s')
                 .help("Only return one PID")
                 .action(ArgAction::SetTrue),
         )
-        .arg(
-            Arg::new("x")
-                .short('x')
-                .help("Return PIDs of shells running scripts with a matching name")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("z")
-                .short('z')
-                .help("List zombie and I/O waiting processes. May cause pidof to hang.")
-                .action(ArgAction::SetTrue),
-        )
+    // .arg(
+    //     Arg::new("x")
+    //         .short('x')
+    //         .help("Return PIDs of shells running scripts with a matching name")
+    //         .action(ArgAction::SetTrue),
+    // )
 }
