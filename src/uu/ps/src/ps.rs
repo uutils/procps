85b04a68b6644bce7ba2f2b248cf34b9b01f8f9d
Krysztal Huang <krysztal.huang@outlook.com>
2024-07-20 22:08:05 +0800

    pidof: Add some basic arguments and flags

--- src/uu/ps/src/ps.rs
+++ src/uu/ps/src/ps.rs
@@ -25,10 +25,75 @@ pub fn uu_app() -> Command {
         .override_usage(format_usage(USAGE))
         .infer_long_args(true)
         .disable_help_flag(true)
-        .arg(
-            Arg::new("help")
-                .long("help")
-                .help("Print help information")
-                .action(ArgAction::Help),
-        )
+        .arg(Arg::new("help").long("help").action(ArgAction::Help))
+        .args([
+            Arg::new("A")
+                .short('A')
+                .help("all processes")
+                .visible_alias("e")
+                .action(ArgAction::SetTrue),
+            Arg::new("a")
+                .short('a')
+                .help("all with tty, except session leaders")
+                .action(ArgAction::SetTrue),
+            // Arg::new("a_")
+            //     .short('a')
+            //     .help("all with tty, including other users")
+            //     .action(ArgAction::SetTrue)
+            //     .allow_hyphen_values(true),
+            Arg::new("d")
+                .short('d')
+                .help("all except session leaders")
+                .action(ArgAction::SetTrue),
+            Arg::new("deselect")
+                .long("deselect")
+                .short('N')
+                .help("negate selection")
+                .action(ArgAction::SetTrue),
+            // Arg::new("r")
+            //     .short('r')
+            //     .action(ArgAction::SetTrue)
+            //     .help("only running processes")
+            //     .allow_hyphen_values(true),
+            // Arg::new("T")
+            //     .short('T')
+            //     .action(ArgAction::SetTrue)
+            //     .help("all processes on this terminal")
+            //     .allow_hyphen_values(true),
+            // Arg::new("x")
+            //     .short('x')
+            //     .action(ArgAction::SetTrue)
+            //     .help("processes without controlling ttys")
+            //     .allow_hyphen_values(true),
+        ])
+    // .args([
+    //     Arg::new("command").short('c').help("command name"),
+    //     Arg::new("GID")
+    //         .short('G')
+    //         .long("Group")
+    //         .help("real group id or name"),
+    //     Arg::new("group")
+    //         .short('g')
+    //         .long("group")
+    //         .help("session or effective group name"),
+    //     Arg::new("PID").short('p').long("pid").help("process id"),
+    //     Arg::new("pPID").long("ppid").help("parent process id"),
+    //     Arg::new("qPID")
+    //         .short('q')
+    //         .long("quick-pid")
+    //         .help("process id"),
+    //     Arg::new("session")
+    //         .short('s')
+    //         .long("sid")
+    //         .help("session id"),
+    //     Arg::new("t").short('t').long("tty").help("terminal"),
+    //     Arg::new("eUID")
+    //         .short('u')
+    //         .long("user")
+    //         .help("effective user id or name"),
+    //     Arg::new("rUID")
+    //         .short('U')
+    //         .long("User")
+    //         .help("real user id or name"),
+    // ])
 }
