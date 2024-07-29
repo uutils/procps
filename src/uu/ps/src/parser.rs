66c580ef1addba48802b1cb692cad24c3a231f37
Krysztal Huang <krysztal.huang@outlook.com>
2024-07-30 02:06:13 +0800

    ps: Add license header for `parser.rs`

--- src/uu/ps/src/parser.rs
+++ src/uu/ps/src/parser.rs
@@ -1,5 +1,9 @@
-use std::convert::Infallible;
+// This file is part of the uutils procps package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
 
+use std::convert::Infallible;
 use thiserror::Error as TError;
 
 #[derive(Debug, TError, PartialEq, Eq)]
