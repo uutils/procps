dbcf821aaa2beea92acdda6a2c7d2c493b2bf110
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2024-07-31 10:31:12 +0200

    free: remove two redundant tests

--- tests/by-util/test_free.rs
+++ tests/by-util/test_free.rs
@@ -17,15 +17,50 @@ fn test_invalid_arg() {
 
 #[test]
 fn test_no_args() {
-    let result = new_ucmd!().succeeds();
-    assert!(result.stdout_str().contains("Mem:"));
+    let re_head_str = r"^ {15}total {8}used {8}free {6}shared {2}buff/cache {3}available$";
+    let re_mem_str = r"^Mem:( +\d+){6}$";
+    let re_swap_str = r"^Swap: ( +\d+){3}$";
+
+    let re_list = vec![
+        Regex::new(re_head_str).unwrap(),
+        Regex::new(re_mem_str).unwrap(),
+        Regex::new(re_swap_str).unwrap(),
+    ];
+
+    let binding = new_ucmd!().succeeds();
+    let free_result = binding.stdout_str();
+    assert_eq!(free_result.len(), 207);
+
+    // Check the format for each line output
+    let mut free_lines = free_result.split('\n');
+    for re in re_list {
+        assert!(re.is_match(free_lines.next().unwrap()));
+    }
 }
 
 #[test]
 fn test_wide() {
-    let result = new_ucmd!().arg("--wide").succeeds();
-    assert!(result.stdout_str().contains("Mem:"));
-    assert!(!result.stdout_str().contains("buff/cache"));
+    let re_head_str = r"^ {15}total {8}used {8}free {6}shared {5}buffers {7}cache {3}available$";
+    let re_mem_str = r"^Mem:( +\d+){7}$";
+    let re_swap_str = r"^Swap: ( +\d+){3}$";
+
+    let re_list = vec![
+        Regex::new(re_head_str).unwrap(),
+        Regex::new(re_mem_str).unwrap(),
+        Regex::new(re_swap_str).unwrap(),
+    ];
+
+    let binding = new_ucmd!().arg("--wide").succeeds();
+    let free_result = binding.stdout_str();
+
+    // The total number of character is always fixed
+    assert_eq!(free_result.len(), 231);
+
+    // Check the format for each line output
+    let mut free_lines = free_result.split('\n');
+    for re in re_list {
+        assert!(re.is_match(free_lines.next().unwrap()));
+    }
 }
 
 #[test]
@@ -74,52 +109,4 @@ fn test_always_one_line() {
     let stdout = result.stdout_str().lines().collect::<Vec<&str>>();
     assert_eq!(stdout.len(), 1);
     assert!(stdout[0].starts_with("SwapUse"));
-}
-
-#[test]
-fn test_column_format() {
-    let re_head_str = r"^ {15}total {8}used {8}free {6}shared {2}buff/cache {3}available$";
-    let re_mem_str = r"^Mem:( +\d+){6}$";
-    let re_swap_str = r"^Swap: ( +\d+){3}$";
-
-    let re_list = vec![
-        Regex::new(re_head_str).unwrap(),
-        Regex::new(re_mem_str).unwrap(),
-        Regex::new(re_swap_str).unwrap(),
-    ];
-
-    let binding = new_ucmd!().succeeds();
-    let free_result = binding.stdout_str();
-    assert_eq!(free_result.len(), 207);
-
-    // Check the format for each line output
-    let mut free_lines = free_result.split('\n');
-    for re in re_list {
-        assert!(re.is_match(free_lines.next().unwrap()));
-    }
-}
-
-#[test]
-fn test_wide_column_format() {
-    let re_head_str = r"^ {15}total {8}used {8}free {6}shared {5}buffers {7}cache {3}available$";
-    let re_mem_str = r"^Mem:( +\d+){7}$";
-    let re_swap_str = r"^Swap: ( +\d+){3}$";
-
-    let re_list = vec![
-        Regex::new(re_head_str).unwrap(),
-        Regex::new(re_mem_str).unwrap(),
-        Regex::new(re_swap_str).unwrap(),
-    ];
-
-    let binding = new_ucmd!().arg("--wide").succeeds();
-    let free_result = binding.stdout_str();
-
-    // The total number of character is always fixed
-    assert_eq!(free_result.len(), 231);
-
-    // Check the format for each line output
-    let mut free_lines = free_result.split('\n');
-    for re in re_list {
-        assert!(re.is_match(free_lines.next().unwrap()));
-    }
 }
