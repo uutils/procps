1f29c74794c16b35462cc2a1fe48253c69312e34
Krysztal Huang <krysztal.huang@outlook.com>
2024-08-08 00:32:10 +0800

    pidwait: Implemented pattern initialize

--- tests/by-util/test_pidwait.rs
+++ tests/by-util/test_pidwait.rs
@@ -11,10 +11,30 @@ fn test_invalid_arg() {
 }
 
 #[test]
+fn test_non_matching_pattern() {
+    new_ucmd!()
+        .arg("THIS_PATTERN_DOES_NOT_MATCH")
+        .fails()
+        .code_is(1)
+        .stderr_contains("pidwait: pattern that searches for process name longer than 15 characters will result in zero matches");
+}
+
+#[test]
 fn test_no_args() {
     new_ucmd!()
         .fails()
         .code_is(2)
         .no_stdout()
         .stderr_contains("no matching criteria specified");
+}
+
+#[test]
+fn test_too_many_patterns() {
+    new_ucmd!()
+        .arg("sh")
+        .arg("sh")
+        .fails()
+        .code_is(2)
+        .no_stdout()
+        .stderr_contains("only one pattern can be provided");
 }
