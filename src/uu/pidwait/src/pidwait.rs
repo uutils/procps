024bdf1bdc8ed88f45e6524f3314cfef1936f940
Krysztal Huang <krysztal.huang@outlook.com>
2024-08-07 23:33:37 +0800

    pidwait: Add command `pidwait`
    And also introduce dependency `polling` for polling `waitpid` syscall

---
+++ src/uu/pidwait/src/pidwait.rs
@@ -0,0 +1,133 @@
+// This file is part of the uutils procps package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+use clap::{arg, crate_version, ArgMatches, Command};
+use regex::Regex;
+use std::{collections::HashSet, env, sync::OnceLock};
+use uu_pgrep::process::{walk_process, ProcessInformation, RunState, Teletype};
+use uucore::{
+    error::{UResult, USimpleError},
+    format_usage, help_about, help_usage,
+};
+
+const ABOUT: &str = help_about!("pidwait.md");
+const USAGE: &str = help_usage!("pidwait.md");
+
+static REGEX: OnceLock<Regex> = OnceLock::new();
+
+#[derive(Debug)]
+struct Settings {
+    echo: bool,
+    count: bool,
+    full: bool,
+    ignore_case: bool,
+    newest: bool,
+    oldest: bool,
+    older: usize,
+    terminal: HashSet<Teletype>,
+    exact: bool,
+    runstates: HashSet<RunState>,
+}
+
+#[uucore::main]
+pub fn uumain(args: impl uucore::Args) -> UResult<()> {
+    let matches = uu_app().try_get_matches_from(args)?;
+
+    let settings = Settings {
+        echo: matches.get_flag("echo"),
+        count: matches.get_flag("count"),
+        full: matches.get_flag("full"),
+        ignore_case: matches.get_flag("ignore-case"),
+        newest: matches.get_flag("newest"),
+        oldest: matches.get_flag("oldest"),
+        older: matches
+            .get_one::<usize>("older")
+            .copied()
+            .unwrap_or_default(),
+        terminal: matches
+            .get_many("terminal")
+            .unwrap_or_default()
+            .cloned()
+            .collect(),
+        exact: matches.get_flag("exact"),
+        runstates: matches
+            .get_many("terminal")
+            .unwrap_or_default()
+            .cloned()
+            .collect(),
+    };
+
+    let pattern = try_get_pattern_from(&matches, &settings)?;
+    REGEX
+        .set(Regex::new(&pattern).map_err(|e| USimpleError::new(2, e.to_string()))?)
+        .unwrap();
+
+    collect_proc_infos(settings);
+
+    Ok(())
+}
+
+fn try_get_pattern_from(matches: &ArgMatches, settings: &Settings) -> UResult<String> {
+    let pattern = match matches.get_many::<String>("pattern") {
+        Some(patterns) if patterns.len() > 1 => {
+            return Err(USimpleError::new(
+                2,
+                "only one pattern can be provided\nTry `pidwait --help' for more information.",
+            ))
+        }
+        Some(mut patterns) => patterns.next().unwrap(),
+        None => return Ok(String::new()),
+    };
+
+    let pattern = if settings.ignore_case {
+        &pattern.to_lowercase()
+    } else {
+        pattern
+    };
+
+    let pattern = if settings.exact {
+        &format!("^{}$", pattern)
+    } else {
+        pattern
+    };
+
+    Ok(pattern.to_string())
+}
+
+fn collect_proc_infos(settings: Settings) -> Vec<ProcessInformation> {
+    let proc_infos: Vec<_> = walk_process().collect();
+
+    proc_infos
+}
+
+#[allow(clippy::cognitive_complexity)]
+pub fn uu_app() -> Command {
+    Command::new(env!("CARGO_PKG_NAME"))
+        .version(crate_version!())
+        .about(ABOUT)
+        .override_usage(format_usage(USAGE))
+        .infer_long_args(true)
+        .args([
+            arg!(-e --echo                      "display PIDs before waiting"),
+            arg!(-c --count                     "count of matching processes"),
+            arg!(-f --full                      "use full process name to match"),
+            // arg!(-g --pgroup        <PGID>      "match listed process group IDs"),
+            // arg!(-G --group         <GID>       "match real group IDs"),
+            arg!(-i --"ignore-case"             "match case insensitively"),
+            arg!(-n --newest                    "select most recently started"),
+            arg!(-o --oldest                    "select least recently started"),
+            arg!(-O --older         <seconds>   "select where older than seconds"),
+            // arg!(-P --parent        <PPID>      "match only child processes of the given parent"),
+            // arg!(-s --session       <SID>       "match session IDs"),
+            arg!(-t --terminal      <tty>       "match by controlling terminal"),
+            // arg!(-u --euid          <ID>        "match by effective IDs"),
+            // arg!(-U --uid           <ID>        "match by real IDs"),
+            arg!(-x --exact                     "match exactly with the command name"),
+            // arg!(-F --pidfile       <file>      "read PIDs from file"),
+            // arg!(-L --logpidfile                "fail if PID file is not locked"),
+            arg!(-r --runstates     <state>     "match runstates [D,S,Z,...]"),
+            // arg!(-A --"ignore-ancestors"        "exclude our ancestors from results"),
+        ])
+}
