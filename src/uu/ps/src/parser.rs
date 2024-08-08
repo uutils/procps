1f24e5487ed82bfeb191ba2bbd7e6d294941bd02
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2024-08-08 17:06:22 +0200

    ps: remove dead code

--- src/uu/ps/src/parser.rs
+++ src/uu/ps/src/parser.rs
@@ -4,16 +4,6 @@
 // file that was distributed with this source code.
 
 use std::convert::Infallible;
-use thiserror::Error as TError;
-
-#[derive(Debug, TError, PartialEq, Eq)]
-pub enum Error {
-    #[error("empty value")]
-    EmptyValue,
-
-    #[error("parsing failed")]
-    ParsingFailed,
-}
 
 /// Parsing _**optional**_ key-value arguments
 ///
@@ -53,34 +43,12 @@ impl OptionalKeyValue {
         }
     }
 
-    pub fn with_key<T>(key: T) -> Self
-    where
-        T: Into<String>,
-    {
-        Self {
-            key: key.into(),
-            value: None,
-        }
-    }
-
     pub fn key(&self) -> &str {
         &self.key
     }
 
     pub fn value(&self) -> &Option<String> {
         &self.value
-    }
-
-    pub fn is_value_empty(&self) -> bool {
-        self.value.is_none()
-    }
-
-    pub fn try_get<T: std::str::FromStr>(&self) -> Result<T, Error> {
-        let Some(ref value) = self.value else {
-            return Err(Error::EmptyValue);
-        };
-
-        value.parse::<T>().map_err(|_| Error::ParsingFailed)
     }
 }
 
@@ -102,35 +70,9 @@ mod tests {
     }
 
     #[test]
-    fn test_parsing() {
-        assert!(new("value").is_value_empty());
-        assert!(!new("value=").is_value_empty());
-        assert!(!new("value=v").is_value_empty());
-        assert!(!new("value=?:").is_value_empty());
-
-        assert!(OptionalKeyValue::with_key("key").key().eq("key"));
-    }
-
-    #[test]
     fn test_get_key() {
         assert_eq!(new("value").key(), "value");
         assert_eq!(new("value=").key(), "value");
         assert_eq!(new("value=?").key(), "value");
-    }
-
-    #[test]
-    fn test_get_value() {
-        // String test
-        assert_eq!(new("value").try_get::<String>(), Err(Error::EmptyValue));
-        assert_eq!(new("value=").try_get::<String>(), Ok("".into()));
-        assert_eq!(new("value=?").try_get::<String>(), Ok("?".into()));
-
-        // Number test
-        assert_eq!(new("value").try_get::<usize>(), Err(Error::EmptyValue));
-        assert_eq!(new("value=0").try_get::<usize>(), Ok(0));
-        assert_eq!(new("value=0").try_get::<i128>(), Ok(0));
-        assert_eq!(new("value=-1").try_get::<i128>(), Ok(-1));
-        assert_eq!(new("value=0").try_get::<u128>(), Ok(0));
-        assert_eq!(new("value=-1").try_get::<u128>(), Err(Error::ParsingFailed));
     }
 }
