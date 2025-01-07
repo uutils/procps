61e91cbdf423e00896f5a9ab8f8276f7ef5aa835
MCredbear <2363764471@qq.com>
2025-01-07 16:51:52 +0800

    pkill: fix CI failures for windows

--- tests/by-util/test_pkill.rs
+++ tests/by-util/test_pkill.rs
@@ -3,6 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+#[cfg(unix)]
 use crate::common::util::TestScenario;
 
 #[cfg(unix)]
