ab6e081faa0a8454e576706c7f2fdab8abf663c2
MCredbear <40625974+MCredbear@users.noreply.github.com>
2025-01-15 22:51:53 +0800

    pkill: implement (#290)
    * pkill: init source
    
    * pkill: implement
    
    * pkill: implement `-e`
    
    * pkill: change `&mut self` to `&self` for `status`, `stat`, `start_time`'s args
    
    * pkill: implement `-H`
    
    * pkill: format code
    
    * pkill: remove .idea dir
    
    * pkill: beautify code
    
    * pkill: beautify code
    
    * pkill: fix Cargo.toml
    
    * pkill: remove duplicated process.rs
    
    * pkill: add unit tests
    
    * pkill: beautify code
    
    * pkill: fix CI failures for windows
    
    * pkill: fix CI failures for windows
    
    * pkill: fix CI failures for windows
    
    * pkill: fix CI failures for windows
    
    * pkill: fix CI failures for windows
    
    * pkill: fix CI failures for MacOS
    
    * pkill: fix CI failures for windows
    
    * pkill: fix CI failures for windows
    
    ---------
    
    Co-authored-by: Krysztal Huang <krysztal.huang@outlook.com>
    Co-authored-by: Sylvestre Ledru <sylvestre@debian.org>

---
+++ tests/by-util/test_pkill.rs
@@ -0,0 +1,51 @@
+// This file is part of the uutils procps package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+#[cfg(unix)]
+use crate::common::util::TestScenario;
+
+#[cfg(unix)]
+#[test]
+fn test_no_args() {
+    new_ucmd!()
+        .fails()
+        .code_is(2)
+        .no_stdout()
+        .stderr_contains("no matching criteria specified");
+}
+
+#[cfg(unix)]
+#[test]
+fn test_non_matching_pattern() {
+    new_ucmd!()
+        .arg("THIS_PATTERN_DOES_NOT_MATCH")
+        .fails()
+        .code_is(1)
+        .no_output();
+}
+
+#[cfg(unix)]
+#[test]
+fn test_too_many_patterns() {
+    new_ucmd!()
+        .arg("sh")
+        .arg("sh")
+        .fails()
+        .code_is(2)
+        .no_stdout()
+        .stderr_contains("only one pattern can be provided");
+}
+
+#[cfg(unix)]
+#[test]
+fn test_invalid_arg() {
+    new_ucmd!().arg("--definitely-invalid").fails().code_is(1);
+}
+
+#[cfg(unix)]
+#[test]
+fn test_help() {
+    new_ucmd!().arg("--help").succeeds();
+}
