aae5099cf287d0e8cb1b010e229e5514c37d9b0f
Bluemangoo <chenfy2006@qq.com>
2025-01-23 20:37:53 +0800

    top: add licence header

--- src/uu/top/src/header.rs
+++ src/uu/top/src/header.rs
@@ -1,3 +1,8 @@
+// This file is part of the uutils procps package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 use crate::picker::{sysinfo, systemstat};
 use bytesize::ByteSize;
 use clap::ArgMatches;
