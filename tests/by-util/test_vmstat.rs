389a9f6dd2f465b9356b4b29d70db8dbeb1bba73
Bluemangoo <chenfy2006@qq.com>
2025-04-16 23:36:14 +0800

    vmstat: more tests

--- tests/by-util/test_vmstat.rs
+++ tests/by-util/test_vmstat.rs
@@ -3,6 +3,8 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+#[cfg(target_os = "linux")]
+use std::time::Duration;
 use uutests::new_ucmd;
 use uutests::util::TestScenario;
 use uutests::util_name;
@@ -18,6 +20,12 @@ fn test_invalid_arg() {
 }
 
 #[test]
+fn test_invalid_number() {
+    new_ucmd!().arg("-1").fails().code_is(1);
+    new_ucmd!().arg("0").fails().code_is(1);
+}
+
+#[test]
 fn test_unit() {
     new_ucmd!().args(&["-S", "M"]).succeeds();
 }
@@ -26,4 +34,38 @@ fn test_unit() {
 #[cfg(target_os = "linux")]
 fn test_invalid_unit() {
     new_ucmd!().args(&["-S", "x"]).fails().code_is(1);
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_header() {
+    let result = new_ucmd!().succeeds();
+    assert!(result.stdout_str().starts_with("procs"));
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_wide_mode() {
+    let result = new_ucmd!().arg("-w").succeeds();
+    assert!(result.stdout_str().starts_with("--procs--"));
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_no_first() {
+    let time = std::time::Instant::now();
+    new_ucmd!().arg("-y").succeeds();
+    assert!(time.elapsed() >= Duration::from_secs(1));
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_active() {
+    let result = new_ucmd!().arg("-a").succeeds();
+    assert!(result
+        .stdout_str()
+        .lines()
+        .nth(1)
+        .unwrap()
+        .contains("active"));
 }
